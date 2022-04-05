83163bb466ccfea8e249191d400a3c6ef28ab7d6
meetkool <lcs2021023@iiitl.ac.in>
2022-04-05 09:17:35 +0530

    serverdb

--- serverdb/models/Users.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const usersSchema = new mongoose.Schema({
-    email: String,
-    password: String
-});
-const User = new mongoose.model("User", usersSchema);
-module.exports = User;
